bce0922e0fad4fb9f516b38b06c47eef3fb821ed
wanghao@cloudcore.cn <qiong1314>
2019-12-13 16:16:18 +0800

    Initial commit

---
+++ src/main/java/com/runqing/security/SecurityMd5Application.java
@@ -0,0 +1,13 @@
+package com.runqing.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMd5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMd5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/runqing/security/SecurityMd5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runqing.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMd5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
